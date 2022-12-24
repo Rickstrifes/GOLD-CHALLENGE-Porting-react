889b13b27ae910e30610f6d334cf4dafd33fac32
Rico Samuel <samuelricoss@yahoo.co.id>
2022-12-24 14:47:48 +0700

    navigate/belumfix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from 'react-router-dom';
 import { router } from './config/Router';
+import { Route } from "react-router-dom";
 import Navigation from './Components/Navigation';
 import Footer from './Components/Footer';
+import Hero from './Components/Hero';
+import Ourservices from './Components/Ourservices';
+import Whyus from './Components/Whyus';
+import Testimoni from './Components/Testimoni';
+import Faqaccordion from './Components/Faqaccordion';
 
 function App() {
   return (
-    <div>
-    <Navigation />
-    <RouterProvider router={router} />
-    <Footer />
-    </div>
+    <BrowserRouter>
+      <RouterProvider router={router}>
+        <Navigation />
+        <Route exact path="/" component={Hero} />
+        <Route path="/ourservices" component={Ourservices} />
+        <Route path="/whyus" component={Whyus} />
+        <Route path="/testimoni" component={Testimoni} />
+        <Route path="/faqaccordion" component={Faqaccordion} />
+        <Footer />
+      </RouterProvider>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,15 +1,16 @@
 import {Container, Nav, Navbar, Offcanvas} from 'react-bootstrap';
 import './Navigation.css';
 import React from 'react';
-
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom'; 
 
 const Navigation = () => {
   return (
-    <>
+    <Fragment>
       {['sm'].map((expand) => (
         <Navbar key={expand} expand={expand} className=" background  fixed-top">
           <Container>
-            <Navbar.Brand href="#" className='navbar-brand'></Navbar.Brand>
+            <Navbar.Brand className='navbar-brand'></Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
@@ -22,17 +23,17 @@ const Navigation = () => {
               </Offcanvas.Header>
               <Offcanvas.Body>
                   <Nav className="justify-content-end flex-grow-1 mb-2 me-5 text-link-style fw-bold">
-                  <Nav.Link href="#" className='nav-link active nav-item px-3'>Our Services</Nav.Link>
-                  <Nav.Link href="#" className='nav-link active nav-item px-3'>Why Us</Nav.Link>
-                  <Nav.Link href="#" className='nav-link active nav-item px-3'>Testimonial</Nav.Link>
-                  <Nav.Link href="#" className='nav-link active nav-item px-3'>FAQ</Nav.Link>
+                  <Link to="/Ourservices" className='nav-link active nav-item px-3'>Our Services</Link>
+                  <Link to="/Whyus" className='nav-link active nav-item px-3'>Why Us</Link>
+                  <Link to="/Testimoni" className='nav-link active nav-item px-3'>Testimonial</Link>
+                  <Link to="/Faqaccordion" className='nav-link active nav-item px-3'>FAQ</Link>
                   </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
         </Navbar>
       ))}
-    </>
+    </Fragment>
   );
 }
 
